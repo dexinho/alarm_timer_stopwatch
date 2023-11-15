dee7b60040539dbcc15bcc217cee6f9f785b6dac
Jasmin Hidanovic <dexinho008@gmail.com>
2023-11-15 22:25:53 +0100

    fixed observer to now trigger more than 1 child

--- src/utility_tools/alarmChangeObserver.js
+++ src/utility_tools/alarmChangeObserver.js
@@ -1,7 +1,7 @@
 import { createdAlarmsDiv } from "./querySelectors.js";
 
 const alarmChildChange = () => {
-  if (createdAlarmsDiv.childElementCount > 0)
+  if (createdAlarmsDiv.childElementCount > 1)
     createdAlarmsDiv.style.display = "flex";
   else createdAlarmsDiv.style.display = "none";
 };
