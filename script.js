f4b25de61a93531ea15d06b77a2881d53d63e050
Jasmin Hidanovic <dexinho008@gmail.com>
2023-11-17 03:30:36 +0100

    updated function calls for timer and stopwatch

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 import startClock from "./src/utility_tools/updateClock.js";
 import timekeepingSelection from "./src/utility_tools/timekeepingSelection.js";
 import dateAndTimeAutocomplete from "./src/utility_tools/dateAndTimeAutocomplete.js";
-import alarmValidation from "./src/utility_tools/alarmValidation.js";
+import { alarmSetup } from "./src/utility_tools/alarmSetup.js";
 import alarmChangeObserver from "./src/utility_tools/alarmChangeObserver.js";
+import stopwatchSetup from "./src/utility_tools/stopwatchSetup.js";
+import timerSetup from "./src/utility_tools/timerSetup.js";
 
 startClock();
 timekeepingSelection();
 dateAndTimeAutocomplete();
-alarmValidation();
+alarmSetup();
+stopwatchSetup()
+timerSetup()
