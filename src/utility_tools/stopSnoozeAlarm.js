c9e7777c9c9a97704869bc4a5b947813c8d32558
Jasmin Hidanovic <dexinho008@gmail.com>
2023-11-19 03:12:48 +0100

    fixed bug with eventListeners being created on each function call

--- src/utility_tools/stopSnoozeAlarm.js
+++ src/utility_tools/stopSnoozeAlarm.js
@@ -10,45 +10,50 @@ const stopSnoozeAlarm = ({
   checkedRepeat,
   alarmID,
 }) => {
-  alarmRingingDecision.forEach((decision) => {
-    decision.addEventListener("click", () => {
-      stopRinging();
-      let currentDate = new Date();
-
-      if (decision.id === "turn-off-alarm") {
-        if (checkedRepeat) {
-          currentDate.setDate(currentDate.getDate() + 1);
-          dateSlotSpan.textContent = currentDate.toLocaleDateString("en-bs");
-        } else {
-          timekeepingDevices.alarms = timekeepingDevices.alarms.reduce(
-            (acc, alarm) => {
-              if (alarm.alarmID !== alarmID) return [...acc, alarm];
-              else return acc;
-            },
-            []
-          );
-          return;
-        }
-      } else if (decision.id === "snooze-alarm") {
-        currentDate.setMinutes(currentDate.getMinutes() + 5);
-      }
-
-      const alarmIndex = timekeepingDevices.alarms.findIndex(
-        (alarm) => alarm.alarmID === alarmID
-      );
-
-      timekeepingDevices.alarms[alarmIndex].alarmInterval = createAlarmInterval(
-        {
-          alarmID,
-          timeLeftSpan,
-          dateSlotSpan,
-          timeSlotSpan,
-          alarmDate: currentDate.getTime(),
-          checkedRepeat,
-        }
-      );
-    });
+  alarmRingingDecision.forEach((decisionBtn) => {
+    decisionBtn.addEventListener("click", btnHandler);
   });
+
+  function btnHandler() {
+    stopRinging();
+    let currentDate = new Date();
+    if (this.id === "turn-off-alarm") {
+      if (checkedRepeat) {
+        currentDate.setDate(currentDate.getDate() + 1);
+        dateSlotSpan.textContent = currentDate.toLocaleDateString("bs");
+      } else {
+        timekeepingDevices.alarms = timekeepingDevices.alarms.reduce(
+          (acc, alarm) => {
+            if (alarm.alarmID !== alarmID) return [...acc, alarm];
+            else return acc;
+          },
+          []
+        );
+        return;
+      }
+    } else if (this.id === "snooze-alarm") {
+      currentDate.setMinutes(currentDate.getMinutes() + 5);
+    }
+
+    alarmRingingDecision.forEach((btn) =>
+      btn.removeEventListener("click", btnHandler)
+    );
+
+    const alarmIndex = timekeepingDevices.alarms.findIndex(
+      (alarm) => alarm.alarmID === alarmID
+    );
+
+    if (alarmIndex !== -1) {
+      timekeepingDevices.alarms[alarmIndex].alarmInterval = createAlarmInterval({
+        alarmID,
+        timeLeftSpan,
+        dateSlotSpan,
+        timeSlotSpan,
+        alarmDate: currentDate.getTime(),
+        checkedRepeat,
+      });
+    }
+  }
 };
 
 const stopRinging = () => {
