67355c58fb49a891fb212eeb1c1161f3f528f368
Jasmin Hidanovic <dexinho008@gmail.com>
2023-11-30 23:54:03 +0100

    removed autocomplete

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 import startClock from "./src/utility_tools/updateClock.js";
 import timekeepingSelection from "./src/utility_tools/timekeepingSelection.js";
-import dateAndTimeAutocomplete from "./src/utility_tools/dateAndTimeAutocomplete.js";
 import { alarmSetup } from "./src/utility_tools/alarmSetup.js";
 import alarmChangeObserver from "./src/utility_tools/alarmChangeObserver.js";
 import stopwatchSetup from "./src/utility_tools/stopwatchSetup.js";
@@ -8,7 +7,6 @@ import timerSetup from "./src/utility_tools/timerSetup.js";
 
 startClock();
 timekeepingSelection();
-dateAndTimeAutocomplete();
 alarmSetup();
 stopwatchSetup()
 timerSetup()
